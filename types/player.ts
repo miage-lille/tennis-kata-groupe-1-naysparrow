a6588a1987e49cf2cb80b131c5fe011dfb5c78e3
Besbas <melik.besbas.etu@univ-lille.fr>
2025-01-31 20:25:14 +0100

    tp terminé

--- types/player.ts
+++ types/player.ts
@@ -1,2 +1,5 @@
 export type Player = 'PLAYER_ONE' | 'PLAYER_TWO';
 export const isSamePlayer = (p1: Player, p2: Player) => p1 === p2;
+
+export const playerOne = (): Player => 'PLAYER_ONE';
+export const playerTwo = (): Player => 'PLAYER_TWO';
